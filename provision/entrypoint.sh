87e0aa016f392fe8e459b41b54928f4248904753
Stipe Čirko <cirko502@gmail.com>
2021-04-19 20:44:26 +0200

    Entrypoint update.

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Start the run once job.
-echo "Docker container has been started"
+while true;
+do
+python3 /app/downloader/downloader.py get_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
+sleep 10;
 
-declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+python3 /app/downloader/downloader.py clean_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
+sleep 10;
 
-# Setup a cron schedule
-echo "SHELL=/bin/bash
-BASH_ENV=/container.env
-* * * * * /usr/local/bin/python3 /app/downloader/downloader.py get_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/python3 /app/downloader/downloader.py clean_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/python3 /app/downloader/downloader.py get_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
-* * * * * /usr/local/bin/python3 /app/downloader/downloader.py clean_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
-# This extra line makes it a valid cron" > scheduler
+python3 /app/downloader/downloader.py get_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
+sleep 10;
 
-crontab scheduler
-cron -f
+python3 /app/downloader/downloader.py clean_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
+sleep 10;
+done
