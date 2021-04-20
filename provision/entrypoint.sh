4a6a9cab35810a181a7b79df8f9af38b0cff6762
Stipe Čirko <cirko502@gmail.com>
2021-04-20 14:17:57 +0200

    Deployment update.

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+sleep 180;
+
 while true;
 do
 python3 /app/downloader/downloader.py get_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 10;
-
-python3 /app/downloader/downloader.py clean_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 10;
+sleep 30;
 
 python3 /app/downloader/downloader.py get_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 10;
+sleep 30;
+
+python3 /app/downloader/downloader.py clean_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
+sleep 30;
 
 python3 /app/downloader/downloader.py clean_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 10;
+sleep 30;
 done
