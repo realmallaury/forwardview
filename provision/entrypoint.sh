cf90b06ae245b52e54aa7360a83c32494d3d2b8d
Stipe Čirko <cirko502@gmail.com>
2021-04-20 22:35:08 +0200

    Adjusted sleep duration.

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -5,14 +5,14 @@ sleep 180;
 while true;
 do
 python3 /app/downloader/downloader.py get_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 30;
+sleep 5;
 
 python3 /app/downloader/downloader.py get_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 30;
+sleep 5;
 
 python3 /app/downloader/downloader.py clean_ticker_list > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 30;
+sleep 5;
 
 python3 /app/downloader/downloader.py clean_ticker_data > /proc/1/fd/1 2>/proc/1/fd/2
-sleep 30;
+sleep 5;
 done
