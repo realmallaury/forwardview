2a7d1164b79020e9d179d60940f00199adf8a14d
Stipe Čirko <cirko502@gmail.com>
2021-04-30 09:49:00 +0200

    Session protection, style update.

--- app/__init__.py
+++ app/__init__.py
@@ -54,6 +54,7 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
+    login_manager.session_protection = "strong"
     login_manager.init_app(app)
 
     @login_manager.user_loader
